125708784f2d2f23c117f6cfb87f7b40352b9533
SwarupVishwas18 <swarupvishwas000@gmail.com>
2023-06-14 22:50:00 +0530

    laptop version completed!

--- js/scripts.js
+++ js/scripts.js
@@ -54,7 +54,7 @@ particlesJS("particles-js", {
   retina_detect: true,
 });
 var count_particles, stats, update;
-stats = new Stats();
+stats = new stats();
 stats.setMode(0);
 stats.domElement.style.position = "absolute";
 stats.domElement.style.left = "0px";
